980081d431aada4d9d53895e49ae7194902f8603
vbello <vbello-tech@gmail.com>
2025-05-13 01:29:35 +0100

    git cache cleared

--- sirmtech/settings/production.py
+++ sirmtech/settings/production.py
@@ -2,4 +2,8 @@ from .settings import *
 
 # settings for production server.
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
--- sirmtech/settings/settings.py
+++ sirmtech/settings/settings.py
@@ -4,6 +4,7 @@ Django settings for sirmtech project.
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -13,10 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qi12u!6n+2_3m9^igpep(5+7s^qv-(k7+9198bfs4gi36al9@o'
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [
     'ayomidebello.pythonanywhere.com',
