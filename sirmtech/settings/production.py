015fb57b41f0a0bf201f03e6cd2951f08d3ad616
vbello <vbello-tech@gmail.com>
2025-05-19 01:03:27 +0100

    settings

--- sirmtech/settings/production.py
+++ sirmtech/settings/production.py
@@ -7,3 +7,11 @@ DEBUG = False
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
