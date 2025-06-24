5b56149c6d8ce1ccd51c3567245651c6ddf1e981
vbello <vbello-tech@gmail.com>
2025-06-24 23:39:25 +0100

    add file log to prod

--- sirmtech/settings/production.py
+++ sirmtech/settings/production.py
@@ -3,7 +3,7 @@ from .settings import *
 # settings for production server.
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
@@ -15,40 +15,4 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
-
-
-
-# LOGGING TO FILE
-
-LOGGING = {
-    "version": 1,
-    'disable_existing_loggers': False,
-    "formatters": {
-        "verbose": {
-            "format": "{name} {levelname} {asctime} {module} {process:d} {thread:d} {message}",
-            "style": "{",
-        },
-        "simple": {
-            "format": "{levelname} {message}",
-            "style": "{",
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'sirmtech.log',
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    },
-    "root": {"level": "INFO", "handlers": ["file"]},
-}
 
