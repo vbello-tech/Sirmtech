ab484845d838c1fb07ae84f5bbc1b65719d0bea9
vbello_tech <vbellotech@gmail.com>
2025-07-17 19:54:55 +0100

    add to cart feature

--- sirmtech/settings/production.py
+++ sirmtech/settings/production.py
@@ -9,7 +9,7 @@ from .settings import *
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db/site.sqlite3",
+        "NAME": BASE_DIR / 'db.sqlite3',
         "OPTIONS": {
             "transaction_mode": "IMMEDIATE",
             "timeout": 5,  # seconds
