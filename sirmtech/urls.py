dcf103133b2dd6f8b928428cb6af15cd254f8ac8
vbello_tech <vbellotech@gmail.com>
2025-07-26 20:17:09 +0100

    refactor: make changes to frontend

--- sirmtech/urls.py
+++ sirmtech/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include("accounts.urls", namespace="user")),
     path('nysc/', include("nysc.urls", namespace="nysc")),
     path('store/', include("store.urls", namespace="store")),
